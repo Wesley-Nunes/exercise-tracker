de09ed0ba7576206b68c76d8c68c8f2e296f95be
Wesley-Nunes <wesnmonteiro@gmail.com>
2024-07-21 23:15:04 -0300

    test: The user creation

--- cypress/e2e/createUser.cy.js
+++ cypress/e2e/createUser.cy.js
@@ -1,5 +1,58 @@
 describe('create an user', () => {
   it('creates an new user', () => {
     cy.visit('/');
+    cy.intercept('POST', '/api/user').as('createUserRequest');
+
+    cy.get('create-user')
+      .shadow()
+      .find('[data-test="create-user-input"]')
+      .type('shut');
+
+    cy.get('create-user')
+      .shadow()
+      .find('[data-test="create-user-btn"]')
+      .click();
+
+    cy.get('create-user')
+      .shadow()
+      .find('[data-test="create-user-result"]')
+      .should('be.visible')
+
+    cy.wait('@createUserRequest').then((interception) => {
+      const result = interception.response.body;
+      expect(result).to.have.property('username');
+      expect(result.username).to.equal('shut');
+      expect(result).to.have.property('_id');
+      expect(result._id).to.be.a('string');
+      expect(result._id).to.not.be.empty;
+    });
+  })
+  it('creates another new user', () => {
+    cy.visit('/');
+    cy.intercept('POST', '/api/user').as('createUserRequest');
+
+    cy.get('create-user')
+      .shadow()
+      .find('[data-test="create-user-input"]')
+      .type('Ravena');
+
+    cy.get('create-user')
+      .shadow()
+      .find('[data-test="create-user-btn"]')
+      .click();
+
+    cy.get('create-user')
+      .shadow()
+      .find('[data-test="create-user-result"]')
+      .should('be.visible')
+
+    cy.wait('@createUserRequest').then((interception) => {
+      const result = interception.response.body;
+      expect(result).to.have.property('username');
+      expect(result.username).to.equal('Ravena');
+      expect(result).to.have.property('_id');
+      expect(result._id).to.be.a('string');
+      expect(result._id).to.not.be.empty;
+    });
   })
 })
