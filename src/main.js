80606e780114af69372ffe5a220e8818a26eea39
Wesley-Nunes <wesnmonteiro@gmail.com>
2024-07-21 23:20:41 -0300

    feat: Update the app
    Add the helmet and body-parser.
    Show the views.
    Update the endpoint to the user creation.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,34 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import helmet from "helmet";
+import bodyParser from 'body-parser';
 import 'dotenv/config';
 
 const app = express();
-const port = process.env.PORT || 3000;
 const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+const __dirname = import.meta.dirname;
 
+
+app.use(helmet());
 app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/views'));
 
-app.listen(port, host, () => console.log(`The app is listening on ${host}:${port}`));
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(`${__dirname}/index.html`));
+});
+
+app.post('/api/user', (req, res) => {
+	const { username } = req.body;
+
+	res.json({ username, _id: "1234" });
+});
+
+app.listen(
+	port,
+	host,
+	() => console.log(`The app is listening on ${host}:${port}`)
+);
