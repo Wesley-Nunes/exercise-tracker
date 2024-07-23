1bcd43e4e545ae4fe5405673270f58183132647d
Wesley-Nunes <wesnmonteiro@gmail.com>
2024-07-23 01:29:03 -0300

    test: Create commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,25 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('createUser', (username) => {
+	cy.get('create-user')
+		.shadow()
+		.find('[data-test="create-user-input"]')
+		.type(username);
+
+	cy.get('create-user')
+		.shadow()
+		.find('[data-test="create-user-btn"]')
+		.click();
+
+	cy.get('create-user')
+		.shadow()
+		.find('[data-test="create-user-input"]')
+		.clear();
+});
+
+Cypress.Commands.add('resultIn', (component) => {
+	cy.get(component)
+		.shadow()
+		.find('result-content')
+		.shadow()
+		.find('[data-test="get-users-result"]')
+})
+
