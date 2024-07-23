eccec7be7ce1c178dc203bd79c4c8daecac93364
Wesley-Nunes <wesnmonteiro@gmail.com>
2024-07-23 01:30:37 -0300

    Create PubSub

---
+++ src/views/PubSub.js
@@ -0,0 +1,27 @@
+const topics = ['users'];
+const PubSub = () => {
+	const subscribersPerTopic = {};
+	const subscribe = (topic, subscriber) => {
+		if (!topics.includes(topic)) {
+			throw new Error(`The topic ${topic} was not found!`);
+		}
+		subscribersPerTopic[topic].push(subscriber);
+	};
+	const publish = (topic, message) => {
+		if (!topics.includes(topic)) {
+			throw new Error(`The topic ${topic} was not found!`);
+		}
+		subscribersPerTopic[topic].forEach((subscriber) => {
+			subscriber(message);
+		});
+	};
+
+	topics.forEach((topic) => {
+		subscribersPerTopic[topic] = [];
+	});
+
+	return { subscribe, publish };
+}
+
+export default PubSub;
+
